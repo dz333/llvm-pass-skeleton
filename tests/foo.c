55b57025574bdb29cd86c078c06b96c73b3c1b17
Drew Zagieboylo <drew@dhcp-rhodes-3407.redrover.cornell.edu>
2019-11-01 14:51:43 -0400

    more test makefile stuff

--- tests/foo.c
+++ tests/foo.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
 static unsigned int y;
 
 void add4(unsigned int n)  {
   if (n < 4) {
     y = y + 4;
   } else {
-    y = y - 4;
+    exit(1);
   }
 }
 
--- tests/getelem.c
+++ tests/getelem.c
@@ -1,9 +1,36 @@
+typedef struct example {
+  int f1;
+  int *f2;
+  char c1;
+  char c2[2];
+} example;
+
+typedef struct lnode {
+  int val;
+  struct lnode *next;
+} lnode;
+
 int doStuff(int n) {
   int temp[10];
   temp[n] = 3;
   return temp[n];
 }
 
+void doList(lnode l, int v) {
+  l.next->val = v;
+}
+
+char doStruct(example n) {
+  return n.c2[1] + n.c1;
+}
+
 int main() {
-  return doStuff(11);
+  example n;
+  n.f1 = 3;
+  n.f2 = &(n.f1);
+  n.c1 = 'a';
+  n.c2[0] = 'b';
+  n.c2[1] = 'c';
+  char x = doStruct(n);
+  return doStuff(x);
 }
